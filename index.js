ed1224eed1b729f78f3cc59dee2ed912f660668d
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-10-28 13:56:43 +0200

    added solutions

--- index.js
+++ index.js
@@ -7,7 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split``.reverse``.join``;
+    /** SOLUTION 1 **/
+    // return str.split``.reverse``.join``;
+
+    /** SOLUTION 2 O(n)**/
+    // let len = str.length - 1;
+    // let reversed = '';
+    // for(let i = len; len >= 0; i--){
+    //     reversed += str[i];
+    // }
+    // return reversed;
+
+    /** SOLUTION 3 O(n)**/
+    // let reversed = '';
+    // for(let character of str){
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+    /** SOLUTION 4 **/
+    return str.split``.reduce((rev, char)=>char + rev,'');
 }
 
 module.exports = reverse;
